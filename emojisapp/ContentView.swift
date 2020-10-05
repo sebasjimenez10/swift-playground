233a3223181ece2196a71e5916ce6f23468046c0
sebasjimenez10 <sebastian@envoy.com>
2020-10-05 17:50:10 -0500

    Initial Commit

---
+++ emojisapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  emojisapp
+//
+//  Created by Sebastian Jimenez on 10/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ emojisapp/emojisappApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojisappApp.swift
+//  emojisapp
+//
+//  Created by Sebastian Jimenez on 10/5/20.
+//
+
+import SwiftUI
+
+@main
+struct emojisappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
